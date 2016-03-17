7ede61c7d070c8e55515d248950a14f6a0cd5bfe
andreashansen <andreashansen1q@gmail.com>
2016-03-17 14:13:58 +0100

    Builder added

---
+++ MonoCrab/CrabBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoCrab
+{
+    class CrabBuilder : IBuilder
+    {
+        private GameObject gameObject;
+
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+
+        public void BuildGameObject(Vector2 position)
+        {
+            gameObject = new GameObject(position);
+            gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.White, 1));
+            gameObject.AddComponent(new CCrab(gameObject));
+            gameObject.AddComponent(new CAnimator(gameObject));
+            gameObject.AddComponent(new CCollider(gameObject, true));
+        }
+    }
+}
---
+++ MonoCrab/OpponentCrabBuilder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MonoCrab
+{
+    class OpponentCrabBuilder : IBuilder
+    {
+        private GameObject gameObject;
+        private Random rnd = new Random();
+        private int selectRnd;
+        public GameObject GetResult()
+        {
+            return gameObject;
+        }
+
+        public void BuildGameObject(Vector2 position)
+        {
+            selectRnd = rnd.Next(10);
+
+            gameObject = new GameObject(position);
+            if (selectRnd == 1)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Yellow, 1));
+            }
+            else if (selectRnd == 2)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Gold, 1));
+            }
+            else if (selectRnd == 3)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Lavender, 1));
+            }
+            else if (selectRnd == 4)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Blue, 1));
+            }
+            else if (selectRnd == 5)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Brown, 1));
+            }
+            else if (selectRnd == 6)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Black, 1));
+            }
+            else if (selectRnd == 7)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Purple, 1));
+            }
+            else if (selectRnd == 8)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Violet, 1));
+            }
+            else if (selectRnd == 9)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Silver, 1));
+            }
+            else if (selectRnd == 10)
+            {
+                gameObject.AddComponent(new CSpriteRenderer(gameObject, "testcrab", Color.Pink, 1));
+            }
+
+            gameObject.AddComponent(new CCrab(gameObject));
+            gameObject.AddComponent(new CAnimator(gameObject));
+            gameObject.AddComponent(new CCollider(gameObject, true));
+        }
+    }
+}
